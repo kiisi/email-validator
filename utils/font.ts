fd540a27a863005b8b95adc9cfb7cf2ed72f905a
kiisi <kiisifelix06@gmail.com>
2024-12-31 02:43:41 +0100

    feat: email ui

---
+++ utils/font.ts
@@ -0,0 +1,16 @@
+import { Allerta_Stencil, Plus_Jakarta_Sans, Sour_Gummy } from "next/font/google";
+
+export const plusJakartaSans = Plus_Jakarta_Sans({
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const sourGummy = Sour_Gummy({
+    weight: ["200", "300", "400", "500", "600", "700", "800"],
+    subsets: ["latin"],
+  });
+  
+  export const allertaStencil = Allerta_Stencil({
+    weight: ["400",],
+    subsets: ["latin"],
+  });
+  
